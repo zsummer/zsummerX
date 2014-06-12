bba29cd7ca2c24515c8e303947e3308fe1fa8aa8
zhangyawei <yawei_zhang@foxmail.com>
2014-06-12 11:40:17 +0800

    add echo stress test

---
+++ example/frameStressTest/FrameStressMain.cpp
@@ -0,0 +1,240 @@
+/*
+ * zsummerX License
+ * -----------
+ * 
+ * zsummerX is licensed under the terms of the MIT license reproduced below.
+ * This means that zsummerX is free software and can be used for both academic
+ * and commercial purposes at absolutely no cost.
+ * 
+ * 
+ * ===============================================================================
+ * 
+ * Copyright (C) 2010-2014 YaweiZhang <yawei_zhang@foxmail.com>.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * ===============================================================================
+ * 
+ * (end of COPYRIGHT)
+ */
+
+//! zsummer的测试服务模块(对应zsummer底层网络封装的上层设计测试服务) 可视为服务端架构中的 gateway服务/agent服务/前端服务, 特点是高并发高吞吐量
+//! main文件
+
+
+#include <zsummerX/FrameHeader.h>
+#include <zsummerX/FrameTcpSessionManager.h>
+#include <zsummerX/FrameMessageDispatch.h>
+using namespace zsummer::log4z;
+
+std::string g_remoteIP = "0.0.0.0";
+unsigned short g_remotePort = 81;
+unsigned short g_startType = 0;  //0 listen, 1 connect
+unsigned short g_maxClient = 1; //0 echo send, 1 direct send
+
+int main(int argc, char* argv[])
+{
+
+#ifndef _WIN32
+	//! linux下需要屏蔽的一些信号
+	signal( SIGHUP, SIG_IGN );
+	signal( SIGALRM, SIG_IGN ); 
+	signal( SIGPIPE, SIG_IGN );
+	signal( SIGXCPU, SIG_IGN );
+	signal( SIGXFSZ, SIG_IGN );
+	signal( SIGPROF, SIG_IGN ); 
+	signal( SIGVTALRM, SIG_IGN );
+	signal( SIGQUIT, SIG_IGN );
+	signal( SIGCHLD, SIG_IGN);
+#endif
+	if (argc == 2 && 
+		(strcmp(argv[1], "--help") == 0 
+		|| strcmp(argv[1], "/?") == 0))
+	{
+		cout <<"please input like example:" << endl;
+		cout << "tcpTest remoteIP remotePort startType maxClient" << endl;
+		cout << "./tcpTest 0.0.0.0 81 0" << endl;
+		cout << "startType: 0 server, 1 client" << endl;
+		cout <<"maxClient: limit max" << endl;
+		return 0;
+	}
+	if (argc > 1)
+	{
+		g_remoteIP = argv[1];
+	}
+	if (argc > 2)
+	{
+		g_remotePort = atoi(argv[2]);
+	}
+	if (argc > 3)
+	{
+		g_startType = atoi(argv[3]);
+	}
+	if (argc > 4)
+	{
+		g_maxClient = atoi(argv[4]);
+	}
+
+
+	
+	if (g_startType == 0)
+	{
+		//! 启动日志服务
+		ILog4zManager::GetInstance()->Config("server.cfg");
+		ILog4zManager::GetInstance()->Start();
+	}
+	else
+	{
+		//! 启动日志服务
+		ILog4zManager::GetInstance()->Config("client.cfg");
+		ILog4zManager::GetInstance()->Start();
+	}
+	LOGI("g_remoteIP=" << g_remoteIP << ", g_remotePort=" << g_remotePort << ", g_startType=" << g_startType 
+		<< ", g_maxClient=" << g_maxClient);
+	ILog4zManager::GetInstance()->SetLoggerLevel(LOG4Z_MAIN_LOGGER_ID, LOG_LEVEL_INFO);
+
+
+	CTcpSessionManager::getRef().Start();
+
+	//test protocols, IDs
+	static const ProtocolID _Heartbeat = 1000;
+	static const ProtocolID _RequestSequence = 1001;
+	static const ProtocolID _ResultSequence = 1002;
+	
+	std::string testStr;
+	testStr.resize(1024, 's');
+
+	if (g_startType) //client
+	{
+		time_t remoteLastHeartbeat = time(NULL);
+		auto connectedfun = [](ConnectorID cID)
+		{
+			LOGI("send to ConnectorID=" << cID << ", msg=hello");
+			WriteStreamPack ws;
+			ws << _RequestSequence << "hello";
+			CTcpSessionManager::getRef().SendOrgConnectorData(cID, ws.GetStream(), ws.GetStreamLen());
+		};
+		auto MyHeartbeat = [&remoteLastHeartbeat](ConnectorID cID)
+		{
+			time_t now = time(NULL);
+			if (now - remoteLastHeartbeat > HEARTBEART_INTERVAL / 1000 * 2)
+			{
+				LOGW("Connector check heartbeat timeout. Connector=" << cID);
+				CTcpSessionManager::getRef().BreakConnector(cID);
+				return;
+			}
+			LOGI("send  heartbeat");
+			WriteStreamPack ws;
+			ws << _Heartbeat;
+			CTcpSessionManager::getRef().SendOrgConnectorData(cID, ws.GetStream(), ws.GetStreamLen());
+		};
+		auto msg_Heartbeat_fun = [&remoteLastHeartbeat](ConnectorID cID, ProtocolID pID, ReadStreamPack & rs)
+		{
+			LOGI("on remote heartbeat");
+			remoteLastHeartbeat = time(NULL);
+		};
+
+
+		auto msg_ResultSequence_fun = [&testStr](ConnectorID cID, ProtocolID pID, ReadStreamPack & rs)
+		{
+			std::string msg;
+			rs >> msg;
+
+
+			WriteStreamPack ws;
+			ws << _RequestSequence << testStr;
+			CTcpSessionManager::getRef().SendOrgConnectorData(cID, ws.GetStream(), ws.GetStreamLen());
+			static unsigned int g_total = 0;
+			g_total++;
+			static unsigned long long g_lasttime = NOW_TIME;
+			if (NOW_TIME - g_lasttime > 5000)
+			{
+				LOGI("per second=" << g_total / 5.0);
+				g_total = 0;
+				g_lasttime = NOW_TIME;
+			}
+			
+		};
+
+
+		CMessageDispatcher::getRef().RegisterOnConnectorEstablished(connectedfun);
+		CMessageDispatcher::getRef().RegisterOnMyConnectorHeartbeatTimer(MyHeartbeat);
+		CMessageDispatcher::getRef().RegisterConnectorMessage(_Heartbeat, msg_Heartbeat_fun);
+		CMessageDispatcher::getRef().RegisterConnectorMessage(_ResultSequence, msg_ResultSequence_fun);
+
+
+
+		tagConnctorConfigTraits traits;
+		traits.cID = 1;
+		traits.remoteIP = "127.0.0.1";
+		traits.remotePort = 81;
+		traits.reconnectInterval = 5000;
+		traits.reconnectMaxCount = 0;
+		CTcpSessionManager::getRef().AddConnector(traits);
+
+	}
+	else
+	{
+		time_t remoteLastHeartbeat = time(NULL);
+		auto MyHeartbeat = [&remoteLastHeartbeat](AccepterID aID, SessionID sID)
+		{
+			time_t now = time(NULL);
+			if (now - remoteLastHeartbeat > HEARTBEART_INTERVAL / 1000 * 2)
+			{
+				LOGW("session heartbeat timeout. AccepterID=" << aID << ", SessionID=" << sID);
+				CTcpSessionManager::getRef().KickSession(aID, sID);
+				return;
+			}
+			LOGI("send  heartbeat");
+			WriteStreamPack ws;
+			ws << _Heartbeat;
+			CTcpSessionManager::getRef().SendOrgSessionData(aID, sID, ws.GetStream(), ws.GetStreamLen());
+		};
+
+		auto msg_Heartbeat_fun = [&remoteLastHeartbeat](AccepterID aID, SessionID sID, ProtocolID pID, ReadStreamPack & rs)
+		{
+			LOGI("on remote heartbeat");
+			remoteLastHeartbeat = time(NULL);
+		};
+		OnSessionMessageFunction msg_RequestSequence_fun = [](AccepterID aID, SessionID sID, ProtocolID pID, ReadStreamPack & rs)
+		{
+			std::string msg;
+			rs >> msg;
+			msg += " echo";
+			WriteStreamPack ws;
+			ws << _ResultSequence << msg;
+			CTcpSessionManager::getRef().SendOrgSessionData(aID, sID, ws.GetStream(), ws.GetStreamLen());
+			
+		};
+		CMessageDispatcher::getRef().RegisterOnMySessionHeartbeatTimer(MyHeartbeat);
+		CMessageDispatcher::getRef().RegisterSessionMessage(_Heartbeat, msg_Heartbeat_fun);
+		CMessageDispatcher::getRef().RegisterSessionMessage(_RequestSequence, msg_RequestSequence_fun);
+
+		tagAcceptorConfigTraits traits;
+		traits.aID = 1;
+		traits.listenPort = 81;
+		traits.maxSessions = 1;
+		traits.whitelistIP.push_back("127.0.");
+		CTcpSessionManager::getRef().AddAcceptor(traits);
+	}
+	CTcpSessionManager::getRef().Run();
+
+	return 0;
+}
+

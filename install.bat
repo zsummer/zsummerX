0cd0d7514f03f608c34448ad6d9688a4b2319534
zhangyawei <yawei_zhang@foxmail.com>
2014-11-12 11:29:56 +0800

    install.bat

---
+++ install.bat
@@ -0,0 +1,6 @@
+xcopy depends\\log4z include\\log4z /s /y  /i
+xcopy depends\\proto4z include\\proto4z /s /y  /i
+xcopy depends\\rc4 include\\rc4 /s /y  /i
+xcopy depends\\log4z\\lib lib /s /y  /i
+
+pause

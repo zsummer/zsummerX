a94fb4fdd10dacfcfa021e9b0d1572cf39bad87c
zhangyawei <yawei_zhang@foxmail.com>
2014-12-13 14:31:55 +0800

    update bat

--- install.bat
+++ install.bat
@@ -1,12 +1,16 @@
+
 xcopy depends\\log4z include\\log4z /s /y  /i
 xcopy depends\\proto4z include\\proto4z /s /y  /i
 xcopy depends\\rc4 include\\rc4 /s /y  /i
 xcopy depends\\log4z\\lib lib /s /y  /i
 
-xcopy include D:\\pj5\\CPP\\depends_win\\include /s /y  /i
-xcopy lib D:\\pj5\\CPP\\depends_win\\lib /s /y  /i
+set destPath=E:\\github\\mini-breeze\\trunk
+rem set destPath=D:\\pj5\\CPP
 
-xcopy include D:\\pj5\\CPP\\depends_linux\\include /s /y  /i
-xcopy lib D:\\pj5\\CPP\\depends_linux\\lib /s /y  /i
+xcopy include %destPath%\\depends_win\\include /s /y  /i
+xcopy lib %destPath%\\depends_win\\lib /s /y  /i
+
+xcopy include %destPath%\\depends_linux\\include /s /y  /i
+xcopy lib %destPath%\\depends_linux\\lib /s /y  /i
 
 pause

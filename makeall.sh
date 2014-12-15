7205ee5cb6e7a7fe958a7ec1b32a4d3c7e374989
zhangyawei <yawei_zhang@foxmail.com>
2014-12-15 13:12:48 +0800

    update shell

--- makeall.sh
+++ makeall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cmake . -DZSUMMERX_USE_SELECT=1 -DCMAKE_BUILD_TYPE=Release
-make -j3
-cmake . -DZSUMMERX_USE_SELECT=1 -DCMAKE_BUILD_TYPE=Debug
-make -j3
+#cmake . -DZSUMMERX_USE_SELECT=1 -DCMAKE_BUILD_TYPE=Release
+#make -j3
+#cmake . -DZSUMMERX_USE_SELECT=1 -DCMAKE_BUILD_TYPE=Debug
+#make -j3
 cmake . -DCMAKE_BUILD_TYPE=Release
 make -j3
 cmake . -DCMAKE_BUILD_TYPE=Debug

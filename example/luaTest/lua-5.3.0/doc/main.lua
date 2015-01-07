aeb10ebd4e673f25b082aa75d34907718ece6a80
zhangyawei <yawei_zhang@foxmail.com>
2015-01-08 00:16:44 +0800

    雏形

--- example/luaTest/lua-5.3.0/doc/main.lua
+++ example/luaTest/lua-5.3.0/doc/main.lua
@@ -2,5 +2,27 @@
 local Proto = require("proto4z")
 print("ssssssss")
 Proto:test()
-local test = {1,2}
-Proto:pack(test, "fdfdsfsd")
+local test = {
+				_id = {val = 1000, type="i32"}, 
+				_user = {val = "user", type="string"},
+				_items = {val = {
+									_itemID = {val = 10, type="ui16"},
+			  						[1] = "_itemID" 
+			  					},
+			  			 type = "table"
+			  			 },
+				[1] = "_id",
+				[2] = "_user",
+				[3] = "_items",
+			 }
+
+print(#test)
+
+
+local data = Proto:pack(test, "table")
+local hex = ""
+for i = 1, #data do
+	hex = hex .. string.format("%02x ",string.byte(data, i))
+end
+print(hex)
+
--- example/luaTest/lua-5.3.0/doc/proto4z.lua
+++ example/luaTest/lua-5.3.0/doc/proto4z.lua
@@ -6,28 +6,28 @@ function Proto:pack(val, tp)
 	print("begin pack")
 	-- base integer type
 	if tp == "i8" then
-		return string.pack("<i8", val)
+		return string.pack("<i1", val)
 	elseif tp == "ui8" then
-		return string.pack("<I8", val)
+		return string.pack("<I1", val)
 
 	elseif tp == "i16" then
-		return string.pack("<i16", val)
+		return string.pack("<i2", val)
 	elseif tp == "ui16" then
-		return string.pack("<I16", val)	
+		return string.pack("<I2", val)	
 
 	elseif tp == "i32" then
-		return string.pack("<i32", val)
+		return string.pack("<i4", val)
 	elseif tp == "ui32" then
-		return string.pack("<I32", val)	
+		return string.pack("<I4", val)	
 
 	elseif tp == "i64" then
-		return string.pack("<i64", val)
+		return string.pack("<i8", val)
 	elseif tp == "ui64" then
-		return string.pack("<I64", val)	
+		return string.pack("<I8", val)	
 
 	-- string type
 	elseif tp == "string" then
-		return string.pack("<s16", val)
+		return string.pack("<s2", val)
 
 	-- float type
 	elseif tp == "float" then
@@ -36,8 +36,17 @@ function Proto:pack(val, tp)
 		return string.pack("<d", val)
 
 	-- user type
+	elseif tp == "table" then
+		local str = ""
+		for i = 1, #val do
+			local v = val[val[i]]
+			if v ~= nil then
+				str = str .. Proto:pack(v.val, v.type)
+			end
+		end
+		return str
 	else
-		print(type(val))
+		print("unknown val type=" .. type(val))
 	end
 end
 

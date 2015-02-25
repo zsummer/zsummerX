405623a31ab432ea722aff801effc0573100f50c
zhangyawei <yawei_zhang@foxmail.com>
2015-02-26 00:27:47 +0800

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-cp -fr ./depends/log4z/ ./include/log4z
-cp -fr ./depends/proto4z/ ./include/proto4z
-cp -fr ./depends/rc4/ ./include/rc4
-cp -fr ./depends/log4z/lib/ ./lib
+cp -vfr ./depends/log4z/ ./include/log4z
+cp -vfr ./depends/proto4z/ ./include/proto4z
+cp -vfr ./depends/rc4/ ./include/rc4
+cp -vfr ./depends/log4z/lib/ ./lib
 destPath="/Users/zhangyawei/Documents/github/breeze"
-cp -fr include/ $destPath"/depends_win/include/"
-cp -fr lib/ $destPath"/depends_win/lib/"
-cp -fr include/ $destPath"/depends_linux/include/"
-cp -fr lib/ $destPath"/depends_linux/lib/"
+cp -vfr ./include/ "$destPath/depends_win/include/"
+cp -vfr ./lib/ "$destPath/depends_win/lib/"
+cp -vfr ./include/ "$destPath/depends_linux/include/"
+cp -vfr ./lib/ "$destPath/depends_linux/lib/"
+cp -vfr ./include/ "$destPath/depends_mac/include/"
+cp -vfr ./lib/ "$destPath/depends_mac/lib/"
+
 echo "success"

3ec23aeffe89ef78c3a1683acbffd03d57274e98
张亚伟 <yawei.zhang@foxmail.com>
2015-09-01 01:42:30 +0800

    lua pack

--- example/bin/client.lua
+++ example/bin/client.lua
@@ -9,43 +9,46 @@ loge = summer.loge
 --echo pack
 
 local echo = {  _iarray = {{_char=1,_uchar=2,_short=3,_ushort=4,_int=5,_uint=6,_i64="12345678",_ui64="12345678"},{_char=1,_uchar=2,_short=3,_ushort=4,_int=5,_uint=6,_i64="12345678",_ui64="12345678"}},
-				_farray = {{_float=2.235,_double=235.111},{_float=2.235,_double=235.111},},
-				_sarray = {{_string="abcdefg"},{_string="abcdefg"},{_string="abcdefg"}},
-				_imap = {{k="123", v={_char=1,_uchar=2,_short=3,_ushort=4,_int=5,_uint=6,_i64="12345678",_ui64="12345678"}}, {k="223", v={_char=1,_uchar=2,_short=3,_ushort=4,_int=5,_uint=6,_i64="12345678",_ui64="12345678"}}},
-				_fmap = {{k="523", v={_float=2.235,_double=235.111}},{k="623", v={_float=2.235,_double=235.111}}},
-				_smap = {{k="723", v={_string="abcdefg"}},{k="823", v={_string="abcdefg"}}},
-				}
+                _farray = {{_float=2.235,_double=235.111},{_float=2.235,_double=235.111},},
+                _sarray = {{_string="abcdefg"},{_string="abcdefg"},{_string="abcdefg"}},
+                _imap = {{k="123", v={_char=1,_uchar=2,_short=3,_ushort=4,_int=5,_uint=6,_i64="12345678",_ui64="12345678"}}, {k="223", v={_char=1,_uchar=2,_short=3,_ushort=4,_int=5,_uint=6,_i64="12345678",_ui64="12345678"}}},
+                _fmap = {{k="523", v={_float=2.235,_double=235.111}},{k="623", v={_float=2.235,_double=235.111}}},
+                _smap = {{k="723", v={_string="abcdefg"}},{k="823", v={_string="abcdefg"}}},
+                }
 
 -- 连接成功事件
 function whenLinked(sID, remoteIP, remotePort)
-	print("session is on connected. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
-	local data = Proto4z.encode(echo, "EchoPack")
-	Proto4z.dump(echo)
-	Proto4z.putbin(data)
-	summer.sendContent(sID, Proto4z.EchoPack.__getID, data)
+    print("session is on connected. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
+    local data = Proto4z.encode(echo, "EchoPack")
+    Proto4z.dump(echo)
+    Proto4z.putbin(data)
+    summer.sendContent(sID, Proto4z.EchoPack.__getID, data)
 end
 summer.whenLinked(whenLinked)
 
 -- 收到消息
 function onMessage(sID, pID, content)
-	--print("onMessage. sID=" .. sID .. ", pID=" .. pID )
-	--Proto4z.putbin(content)
-	local name = Proto4z.getName(pID)
-	if name == nil then
-		logw("unknown message id recv. pID=" .. pID)
-	else
-			local echo = Proto4z.decode(content, name)
-			--Proto4z.dump(echo)
-			local data = Proto4z.encode(echo, "EchoPack")
-			summer.sendContent(sID, Proto4z.EchoPack.__getID, data)
-	end
+    --print("onMessage. sID=" .. sID .. ", pID=" .. pID )
+    --Proto4z.putbin(content)
+    local name = Proto4z.getName(pID)
+    if name == nil then
+        logw("unknown message id recv. pID=" .. pID)
+    else
+            local echo = Proto4z.decode(content, name)
+            --Proto4z.dump(echo)
+            local data = Proto4z.encode(echo, "EchoPack")
+            summer.sendContent(sID, Proto4z.EchoPack.__getID, data)
+            --local data = Proto4z.pack(echo, "EchoPack")
+            --summer.sendData(sID, data)
+            
+    end
 
 end
 summer.whenMessage(onMessage)
 
 -- 连接断开事件
 function whenClosed(sID, remoteIP, remotePort)
-	print("session is on disconnect. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
+    print("session is on disconnect. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
 end
 summer.whenClosed(whenClosed)
 
@@ -56,7 +59,7 @@ summer.start()
 --连接服务器
 local id = summer.addConnect("127.0.0.1", 8081, nil, 10)
 if id == nil then
-	summer.logw("id == nil when addConnect")
+    summer.logw("id == nil when addConnect")
 end
 print("new connect id=" .. id)
 
--- example/bin/server.lua
+++ example/bin/server.lua
@@ -10,24 +10,24 @@ local echoCount = 0
 
 -- 收到消息
 function onMessage(sID, pID, content)
-	--print("onMessage. sID=" .. sID .. ", pID=" .. pID )
-	--Proto4z.putbin(content)
-	local name = Proto4z.getName(pID)
-	if name == nil then
-		logw("unknown message id recv. pID=" .. pID)
-	else
-		--echo
-		summer.sendContent(sID, pID, content)
+    --print("onMessage. sID=" .. sID .. ", pID=" .. pID )
+    --Proto4z.putbin(content)
+    local name = Proto4z.getName(pID)
+    if name == nil then
+        logw("unknown message id recv. pID=" .. pID)
+    else
+        --echo
+        summer.sendContent(sID, pID, content)
 
-		--Proto4z.dump(echo)
-		local echo = Proto4z.decode(content, name)
-		echoCount = echoCount + 1
-		if os.time() - lastTime >=5 then
-			print("per second = " .. echoCount/5)
-			echoCount = 0
-			lastTime = os.time()
-		end
-	end
+        --Proto4z.dump(echo)
+        local echo = Proto4z.decode(content, name)
+        echoCount = echoCount + 1
+        if os.time() - lastTime >=5 then
+            print("per second = " .. echoCount/5)
+            echoCount = 0
+            lastTime = os.time()
+        end
+    end
 
 end
 summer.whenMessage(onMessage)
@@ -35,12 +35,12 @@ summer.whenMessage(onMessage)
 
 -- 连接成功事件
 function whenLinked(sID, remoteIP, remotePort)
-	print("session is on connected. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
+    print("session is on connected. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
 end
 summer.whenLinked(whenLinked)
 -- 连接断开事件
 function whenClosed(sID, remoteIP, remotePort)
-	print("session is on disconnect. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
+    print("session is on disconnect. sID=" .. sID .. ", remoteIP=" .. remoteIP .. ", remotePort=" .. remotePort)
 end
 summer.whenClosed(whenClosed)
 
@@ -51,7 +51,7 @@ summer.start()
 --连接服务器
 local id = summer.addListen("127.0.0.1",8081)
 if id == nil then
-	summer.logw("id == nil when addListen")
+    summer.logw("id == nil when addListen")
 end
 print("new accept id=" .. id)
 

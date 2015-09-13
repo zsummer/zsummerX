985e364219a6fa56a437ee3b288597f73b96f172
张亚伟 <yawei.zhang@foxmail.com>
2015-09-14 07:34:26 +0800

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cp -vfr ./depends/log4z/ ./include/log4z
-cp -vfr ./depends/proto4z/ ./include/proto4z
-cp -vfr ./depends/rc4/ ./include/rc4
-cp -vfr ./depends/log4z/lib/ ./lib
+cp -vfr ./depends/log4z ./include/
+cp -vfr ./depends/proto4z ./include/
+cp -vfr ./depends/rc4 ./include/
+cp -vfr ./depends/log4z/lib ./
 destPath="/Users/zhangyawei/Documents/github/breeze"
-cp -vfr ./include/ "$destPath/depends/include"
-cp -vfr ./lib/ "$destPath/depends_mac/lib"
+cp -vfr ./include "$destPath/depends/"
+cp -vfr ./lib "$destPath/depends_mac/"
 
 echo "success"
